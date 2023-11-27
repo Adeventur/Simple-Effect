d33049f363001c5774591d80233ae7ba7f9deb2c
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-27 12:30:15 +0100

    comment out css and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-        $("p").click(function() {
-          $(".walrus-showing").toggle();
-          $(".walrus-hidden").toggle();
-        });
-      });
+// $(document).ready(function() {
+//         $("p").click(function() {
+//           $(".walrus-showing").toggle();
+//           $(".walrus-hidden").toggle();
+//         });
+//       });
