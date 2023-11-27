58f96db5505ef7386e9556620677344f0712043d
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-27 13:11:52 +0100

    add fadein

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-// $(document).ready(function() {
-//         $("p").click(function() {
-//           $(".walrus-showing").toggle();
-//           $(".walrus-hidden").toggle();
-//         });
-//       });
+$(document).ready(function() {
+        $(".clickable").click(function() {
+          $("#walrus-showing").toggle();
+          $("#walrus-hidden").toggle();
+        });
+        $('#.clickable').fadeIn(1000);
+      });
