add1b61e357fe4c9852c65ea17eee395d1da740e
ADEBISI WASIU <105849469+Adeventur@users.noreply.github.com>
2023-11-27 13:30:29 +0100

    strick

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  // $(".clickable").click(function () {
-  //   $("#walrus-showing").toggle().fadeIn();
-  //   $("#walrus-hidden").toggle().fadeOut();
-  // });
-
   $(".clickable").click(function () {
-    $("#walrus-showing").slideToggle().slideDown();
-    $("#walrus-hidden").slideToggle().slideUp();
+    $("#walrus-showing").toggle().fadeIn();
+    $("#walrus-hidden").toggle().fadeOut();
   });
-});
+
+//   $(".clickable").click(function () {
+//     $("#walrus-showing").slideToggle().slideDown();
+//     $("#walrus-hidden").slideToggle().slideUp();
+//   });
+// })
